61f51b7a69913c9bc12031f4796884414e7a3191
liminal <122311991+liminalityyy@users.noreply.github.com>
2023-02-08 16:34:01 +0200

    fixed gem trees

--- src/custom/WorldGenGemTrees.java
+++ src/custom/WorldGenGemTrees.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class WorldGenGemTrees extends WorldGenerator {
 	public boolean generate(World world1, Random random2, int i3, int i4, int i5) {
-		int i6 = random2.nextInt(2) + 3;
+		int i6 = random2.nextInt(3) + 4;
 		boolean z7 = true;
 		if(i4 >= 1 && i4 + i6 + 1 <= 128) {
 			int i8;
@@ -43,9 +43,9 @@ public class WorldGenGemTrees extends WorldGenerator {
 					world1.setBlock(i3, i4 - 1, i5, Block.dirt.blockID);
 
 					int i16;
-					for(i16 = i4 - 3 * i6; i16 <= i4 + i6; ++i16) {
+					for(i16 = i4 - 3 + i6; i16 <= i4 + i6; ++i16) {
 						i10 = i16 - (i4 + i6);
-						i11 = 1 + i10 / 2;
+						i11 = 1 - i10 / 2;
 
 						for(i12 = i3 - i11; i12 <= i3 + i11; ++i12) {
 							int i13 = i12 - i3;
